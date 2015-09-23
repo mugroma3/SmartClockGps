fe3f3ef5372a6f6627cb998d80563d106575ab0e
John R. D'Orazio <donjohn.fmmi@gmail.com>
2015-09-23 17:32:51 +0200

    changed variables to english

--- SCGbetaVins/SCGbetaVins.ino
+++ SCGbetaVins/SCGbetaVins.ino
@@ -7,7 +7,7 @@ LiquidCrystal lcd(rs, en, 5, 4, 3, 2);
 
 SoftwareSerial BTSerial(13, 12); // RX | TX
 String inputString = "";         // a string to hold incoming data
-String gprmc="$GPRMC";
+String GPSCommandString="$GPRMC";
 String valueArray[13];
 int utc = 2;
 
@@ -22,29 +22,29 @@ void setup() {
 void loop() {
  if(BTSerial.available()>0){
     inputString = BTSerial.readStringUntil('\n');
-    if(inputString.startsWith(gprmc)){
+    if(inputString.startsWith(GPSCommandString)){
       elaborateValues(inputString);
     }
  }
 }
 
 void elaborateValues(String myString){
-  int ore,minuti,secondi;
-  String oreString,minutiString,secondiString;
+  int hours,minutes,seconds;
+  String hourString,minuteString,secondString;
   int commaIndex = myString.indexOf(',');
   int secondCommaIndex = myString.indexOf(',', commaIndex+1);
   String secondValue = myString.substring(commaIndex+1, secondCommaIndex);
-  ore=((secondValue.substring(0,2)).toInt())+utc;
-  minuti=(secondValue.substring(2,4)).toInt();
-  secondi=(secondValue.substring(4,6)).toInt();
-  oreString+=ore;
-  minutiString=secondValue.substring(2,4);
-  secondiString=secondValue.substring(4,6);
-  String orario=oreString+":"+minutiString+":"+secondiString;
+  hours=((secondValue.substring(0,2)).toInt())+utc;
+  minutes=(secondValue.substring(2,4)).toInt();
+  seconds=(secondValue.substring(4,6)).toInt();
+  hourString+=hours;
+  minuteString=secondValue.substring(2,4);
+  secondString=secondValue.substring(4,6);
+  String timeString=hourString+":"+minuteString+":"+secondString;
   Serial.println(secondValue);
-  Serial.println(orario);
+  Serial.println(hourString);
   Serial.println("\n");
   lcd.setCursor(0, 0);
-  lcd.print(orario);
+  lcd.print(hourString);
 }
 
