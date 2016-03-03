a9c12f0b0391db3dbbd1b16400a18bdc7cfb87f5
vins-87 <vincenzo.dorso@gmail.com>
2016-03-03 17:52:08 +0100

    started to create library

--- SmartClockGPS/SmartClock.h
+++ SmartClockGPS/SmartClock.h
@@ -1,6 +1,10 @@
+
+
+#ifndef SmartClock_h
+#define SmartClock_h
+
 //module max inquire devices, can change to optimize HC-05 connectivity
 #define MAX_DEVICES         15
-
 
 enum HC05MODE { COMMUNICATION_MODE=0, AT_MODE }; //0,1 = LOW,HIGH
 enum HC05ROLE { SLAVE=0, MASTER, SLAVE_LOOP };
@@ -52,83 +56,124 @@ enum CHRONOSTATE {
   CHRONOSTOPPED
 };
 
-int dynamicEvent;
-int tickEvent;                      // event that will be called once every second to create the local Arduino clock
-int firstSynch;                     // event that will be called once after 1 second to synch the local Arduino clock with GPS time data
-int synchEvent;                     // event that will be called once every so often to synch the local Arduino clock with GPS time data
-int chronometerEvent;               // event that will be called once every 50 milliseconds as a chronometer function
 
-String GPSValueStrings[7];
+class SmartClockClass{
+
+    public:
+        boolean elaborateGPSValues(String myString);
+        void chronometer(void* context);
+        void updateClock(void* context);
+        void synchTime(void* context);
+        boolean isLeapYear(int year);
+        int dayOfTheWeek(int y, int m, int d);
+        void checkButtonsPressed();
+        void printMenu();
+        void saveSettings();
+        void resetAndExitMenu();
+        void Set_HC05_MODE(void* context);
+        HC05STATE Check_HC05_STATE();
+        void CountRecentAuthenticatedDevices();
+        void CheckMostRecentAuthenticatedDevice();
+        void SearchAuthenticatedDevice(String addr);
+        void ConnectRecentAuthenticatedDevice(String addr);
+        HC05CMODE SetConnectionMode(HC05CMODE mode);
+        void InitiateInquiry();
+        void InquireDevices();
+        void ConfrontUserWithDevice(String devicexAddr);
+        void SetBindAddress();
+        void LinkToCurrentDevice(String devicexAddr);
+        void flushOkString();
+        void initializeAllVariables();
+        void resetAllVariables();
+        String getErrorMessage(String errCodeStr);
+        int inArray(String needle,String* haystack, int len);
+        int knotsToMPH(int knots);
+        int knotsToKMH(int knots);
+        String MapValue(String needle,const String haystack[][2],int len);
+        void initializeEEPROM();
+
+    private:
+
+        int dynamicEvent;
+        int tickEvent;                      // event that will be called once every second to create the local Arduino clock
+        int firstSynch;                     // event that will be called once after 1 second to synch the local Arduino clock with GPS time data
+        int synchEvent;                     // event that will be called once every so often to synch the local Arduino clock with GPS time data
+        int chronometerEvent;               // event that will be called once every 50 milliseconds as a chronometer function
+
+        String GPSValueStrings[7];
 
 
 /**********************************************************************
- * Define Program Level Global variables 
- * (not user changeable, only the program can change their value) 
+ * Define Program Level Global variables
+ * (not user changeable, only the program can change their value)
  * held in RAM memory
  * ********************************************************************
 */
 
-ProgramState OLDPROGRAMSTATE;
-ProgramState CURRENTPROGRAMSTATE;
-boolean PROGRAMSTATECHANGED;
-boolean SETTINGHC05MODE;
-boolean INITIALIZING;
+        ProgramState OLDPROGRAMSTATE;
+        ProgramState CURRENTPROGRAMSTATE;
+        boolean PROGRAMSTATECHANGED;
+        boolean SETTINGHC05MODE;
+        boolean INITIALIZING;
 
-HC05MODE HC05_MODE;                   //can be AT_MODE or COMMUNICATION_MODE
-HC05CMODE currentCMODE;               //can be CONNECT_BOUND, CONNECT_ANY, or CONNECT_SLAVE_LOOP
-HC05STATE HC05_STATE;                 //can be CONNECTED or DISCONNECTED
-HC05STATE HC05_OLDSTATE;              //can be CONNECTED or DISCONNECTED
-CHRONOSTATE ChronoState;              //can be CHRONOINIT, CHRONORUNNING or CHRONOSTOPPED
+        HC05MODE HC05_MODE;                   //can be AT_MODE or COMMUNICATION_MODE
+        HC05CMODE currentCMODE;               //can be CONNECT_BOUND, CONNECT_ANY, or CONNECT_SLAVE_LOOP
+        HC05STATE HC05_STATE;                 //can be CONNECTED or DISCONNECTED
+        HC05STATE HC05_OLDSTATE;              //can be CONNECTED or DISCONNECTED
+        CHRONOSTATE ChronoState;              //can be CHRONOINIT, CHRONORUNNING or CHRONOSTOPPED
 
-LOCALE currentLocale;                 //can be ENG, ITA, ESP, FRA, DEU
-BASEMENU BaseMenu;                    //can be CLOCK, CHRONOMETER, GPSDATA
-int CURRENTMENUITEM;                  //declaring as int rather than MENUITEM so that we can iterate over the items
-int PREVIOUSMENUITEM;                 //declaring as int rather than MENUITEM so that we can iterate over the items
-boolean MENUACTIVE;                   //whether user has entered into main menu
-int MENULEVEL;                        //current level within main menu
-int MENUITEMS;                        //item count in current menu level
-int initcount;                        //for iterating over initphase strings
-int searchcount;                      //for iterating over searchphase and and linkphase strings
+        LOCALE currentLocale;                 //can be ENG, ITA, ESP, FRA, DEU
+        BASEMENU BaseMenu;                    //can be CLOCK, CHRONOMETER, GPSDATA
+        int CURRENTMENUITEM;                  //declaring as int rather than MENUITEM so that we can iterate over the items
+        int PREVIOUSMENUITEM;                 //declaring as int rather than MENUITEM so that we can iterate over the items
+        boolean MENUACTIVE;                   //whether user has entered into main menu
+        int MENULEVEL;                        //current level within main menu
+        int MENUITEMS;                        //item count in current menu level
+        int initcount;                        //for iterating over initphase strings
+        int searchcount;                      //for iterating over searchphase and and linkphase strings
 
-String devices[MAX_DEVICES];          //array of device addresses
-int deviceCount;                      //total device addresses found
-int currentDeviceIdx;                 //index of iteration through device addresses
-String currentDeviceAddr;             //address of device at current index
-String currentDeviceName;             //name of device at current index
-int recentDeviceCount;                //result of recent paired devices count request
-int currentFunctionStep;              //SetHC05Mode function has different steps
-String incoming;                      //for reading incoming Serial data (from HC-05 module to Serial monitor)
-String outgoing;                      //outgoing Serial data (from user / Serial monitor to HC-05 module)
-String GPSCommandString;              //the string that will hold the incoming GPS data stream (one line at a time)
-int currentYear;                      //used for creating clock date-time
-int currentMonth;                     //used for creating clock date-time
-int currentDay;                       //used for creating clock date-time
-int currentHour;                      //used for creating clock date-time
-int currentMinute;                    //used for creating clock date-time
-int currentSecond;                    //used for creating clock date-time
-String timeString;                    //final string of the time to display on the LCD
-String dateString;                    //final string of the data to display on the LCD
-int GPSDataCounter;                   //GPS DATA
+        String devices[MAX_DEVICES];          //array of device addresses
+        int deviceCount;                      //total device addresses found
+        int currentDeviceIdx;                 //index of iteration through device addresses
+        String currentDeviceAddr;             //address of device at current index
+        String currentDeviceName;             //name of device at current index
+        int recentDeviceCount;                //result of recent paired devices count request
+        int currentFunctionStep;              //SetHC05Mode function has different steps
+        String incoming;                      //for reading incoming Serial data (from HC-05 module to Serial monitor)
+        String outgoing;                      //outgoing Serial data (from user / Serial monitor to HC-05 module)
+        String GPSCommandString;              //the string that will hold the incoming GPS data stream (one line at a time)
+        int currentYear;                      //used for creating clock date-time
+        int currentMonth;                     //used for creating clock date-time
+        int currentDay;                       //used for creating clock date-time
+        int currentHour;                      //used for creating clock date-time
+        int currentMinute;                    //used for creating clock date-time
+        int currentSecond;                    //used for creating clock date-time
+        String timeString;                    //final string of the time to display on the LCD
+        String dateString;                    //final string of the data to display on the LCD
+        int GPSDataCounter;                   //GPS DATA
 
-unsigned long currentMillis;          //will count the current milliseconds of the Arduino when the chronometer function is activated
-unsigned long oldtime;                //useful for filtering button press
-unsigned long newtime;                //useful for filtering button press
-int MENUBUTTONSTATE;                  //button state LOW / HIGH
-int NAVIGATEBUTTONSTATE;              //button state LOW / HIGH
-boolean MENUBUTTONPRESSED;            //button press event
-boolean NAVIGATEBUTTONPRESSED;        //button press event
+        unsigned long currentMillis;          //will count the current milliseconds of the Arduino when the chronometer function is activated
+        unsigned long oldtime;                //useful for filtering button press
+        unsigned long newtime;                //useful for filtering button press
+        int MENUBUTTONSTATE;                  //button state LOW / HIGH
+        int NAVIGATEBUTTONSTATE;              //button state LOW / HIGH
+        boolean MENUBUTTONPRESSED;            //button press event
+        boolean NAVIGATEBUTTONPRESSED;        //button press event
 
-int addressIntUTCOffset;
-int addressIntLanguage;
-int addressIntDateView;
-int addressIntSynchFrequency;
-float addressFloatVersion;
+        int addressIntUTCOffset;
+        int addressIntLanguage;
+        int addressIntDateView;
+        int addressIntSynchFrequency;
+        float addressFloatVersion;
 
 /****************************************
  * Define User Preference globals (held in RAM memory)
  * TODO: should be stored in EEPROM!
  ****************************************
  */
-int offsetUTC;
-boolean useLangStrings;
+        int offsetUTC;
+        boolean useLangStrings;
+};
+
+#endif // SmartClock_h
 
